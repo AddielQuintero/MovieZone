e8324491d130af83ce12a53af1cf9c74a13a6c26
atamayo <atamayoquintero@gmail.com>
2023-06-18 12:47:58 -0500

    fix: update Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,16 @@
+import { MovieList, HomeHero } from '@components'
+
 export const Home = () => {
-  return <>Home</>
+  return (
+    <>
+      <div>
+        <HomeHero />
+      </div>
+      <div className="movie flex flex-col gap-y-8">
+        <MovieList title="Popular" redirect="popular" />
+        <MovieList title="Trending" redirect="trending" />
+        <MovieList title="Upcoming" redirect="upcoming" />
+      </div>
+    </>
+  )
 }
