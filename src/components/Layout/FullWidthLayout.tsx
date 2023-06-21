85ba9525522538986bc95b10b6a71706b2566b4a
atamayo <atamayoquintero@gmail.com>
2023-06-21 18:33:50 -0500

    fix: update layout component

--- src/components/Layout/FullWidthLayout.tsx
+++ src/components/Layout/FullWidthLayout.tsx
@@ -5,12 +5,11 @@ interface LayoutProps {
   children: React.ReactNode
 }
 
-export const Layout = ({ children }: LayoutProps) => {
+export const FullWidthLayout = ({ children }: LayoutProps) => {
   return (
-    <div className='layout'>
+    <div>
       <NavBar navigation={navigation} />
-      {/* <div className="container mx-auto mt-5 px-5 pb-5 sm:p-5"> */}
-      {children}
+      <div>{children}</div>
     </div>
   )
 }
