5cb076525e7344f7a92852ab86d9c37741182422
atamayo <atamayoquintero@gmail.com>
2023-06-21 18:32:42 -0500

    fix: update movieDetailComponents

--- src/components/Movie/MovieDetailComponents/MovieBackgroundImage.tsx
+++ src/components/Movie/MovieDetailComponents/MovieBackgroundImage.tsx
@@ -1,11 +1,21 @@
-import { ClassNameProps } from "@types"
+import { MovieImageProps } from '@types'
 
-export const MovieBackgroundImage = ({ className }: ClassNameProps) => {
+export const MovieBackgroundImage = ({ url, className, classImage, classImageReflection,}: MovieImageProps) => {
+
   return (
     <>
-      <div className={`absolute top-0 inset-0 -z-10  bg-[url('https://image.tmdb.org/t/p/original//1inZm0xxXrpRfN0LxwE2TXzyLN6.jpg')] bg-cover bg-top ${className}`}>
-        <div className="inset-0 h-full w-full bg-gradient-to-b from-transparent to-white" />
+      <div className={className}>
+        <img className={`${classImage} w-full h-full object-center`} src={url} alt="" />
       </div>
+      {classImageReflection && (
+        <div className={`${classImageReflection} relative h-10 opacity-40 overflow-hidden px-2`}>
+          <img className="w-full h-36 pt-8 rounded-b-lg -scale-y-100 transform transition-all duration-[.35s]" src={url} alt="" />
+        </div>
+      )}
     </>
   )
 }
+
+   //<div className="inset-0 h-full w-full bg-gradient-to-b from-transparent to-white" /> 
+   //<div className={`absolute top-0 inset-0 -z-10  bg-[url('https://image.tmdb.org/t/p/original//1inZm0xxXrpRfN0LxwE2TXzyLN6.jpg')] bg-cover bg-top ${className}`}>
+
--- src/components/Movie/MovieDetailComponents/MovieCategories.tsx
+++ src/components/Movie/MovieDetailComponents/MovieCategories.tsx
@@ -1,15 +1,13 @@
 import { Chip, Typography } from '@material-tailwind/react'
 
-export const MovieCategories = () => {
+export const MovieCategories = ({ categories }: { categories: string[] }) => {
   return (
     <div>
       <Typography variant="h4">Categories</Typography>
       <div className="flex items-end gap-2 pt-2">
-        {Array(3)
-          .fill({})
-          .map((_, index) => (
-            <Chip className="bg-blue-gray-800" size="lg" value="Action" key={index} />
-          ))}
+        {categories.map((category, index) => (
+          <Chip className="bg-blue-gray-800" size="lg" value={category} key={index} />
+        ))}
       </div>
     </div>
   )
