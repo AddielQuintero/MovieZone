abc02af08ce09f6c1d1dbca50786443d644103e0
atamayo <atamayoquintero@gmail.com>
2023-06-26 10:44:23 -0500

    fix: updated Material Tailwind theme with additional screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,23 +3,34 @@
 const withMT = require('@material-tailwind/react/utils/withMT')
 
 module.exports = withMT({
+  // module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+
   theme: {
     screens: {
-      'sm': '640px',
-      // => @media (min-width: 640px) { ... }
+      sm: '640px',
+      'max-sm': { max: '640px' },
 
-      'md': '768px',
-      // => @media (min-width: 768px) { ... }
+      md: '768px',
+      'max-md': { max: '768px' },
 
-      'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
+      lg: '1024px',
+      'max-lg': { max: '1024px' },
 
-      'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
+      xl: '1280px',
+      'max-xl': { max: '1280px' },
 
       '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
+      'max-2xl': { max: '1536px' },
+
+      '3xl': '1920px',
+      'max-3xl': { max: '1920px' },
+
+      '4xl': '2560px',
+      'max-4xl': { max: '2560px' },
+      
+      '5xl': '3840px',
+      'max-5xl': { max: '3840px' },
     },
     extend: {},
   },
