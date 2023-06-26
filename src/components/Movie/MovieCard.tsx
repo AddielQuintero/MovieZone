2cf760bf8779140d05a358e40dd4055e5c73c24b
atamayo <atamayoquintero@gmail.com>
2023-06-26 10:40:13 -0500

    fix: update movieCard component

--- src/components/Movie/MovieCard.tsx
+++ src/components/Movie/MovieCard.tsx
@@ -1,27 +1,26 @@
 import { Card, CardHeader, CardFooter, Typography, IconButton } from '@material-tailwind/react'
 import { HeartIcon } from '@heroicons/react/24/outline'
 import { Link } from 'react-router-dom'
+import { MovieCardProps } from '@types'
 
-export const MovieCard = () => {
+export const MovieCard = ({ className, classHeader, classFooter, classLink, classTypography, color, textFooter }: MovieCardProps) => {
   return (
-    <Card
-      shadow={false}
-      className="test bg-inherit relative grid h-[19rem] w-full max-w-[12rem]  rounded-none items-end overflow-hidden text-center"
-    >
-      <Link   to={`/movies/category/${1}`} className="">
+    <Card shadow={false} className={className}>
+      <Link to={`/movies/category/${1}`} className={classLink}>
         <CardHeader
           floated={false}
           shadow={false}
           children={false}
-          color="transparent"
-          className="absolute inset-0 m-0 h-[83%] w-full rounded-2xl bg-[url('https://image.tmdb.org/t/p/original//vZloFAK7NmvMGKE7VkF5UHaz0I.jpg')] bg-cover"
+          color={color}        
+          className={classHeader}
+          // style={{backgroundImage: "url('https://image.tmdb.org/t/p/original//1inZm0xxXrpRfN0LxwE2TXzyLN6.jpg')", backgroundSize: "cover"}}
         />
       </Link>
 
-      <CardFooter className="flex items-center justify-between w-full p-2">
-        <Typography className="font-normal">2022</Typography>
-        <IconButton variant="text" color="white">
-          <HeartIcon strokeWidth={2} className="h-5 w-5 text-indigo-500" />
+      <CardFooter className={classFooter}>
+        <Typography className={classTypography}>{textFooter}</Typography>
+        <IconButton variant="text" color="white" className="h-5 w-5">
+          <HeartIcon className="h-5 w-5 text-indigo-500" />
         </IconButton>
       </CardFooter>
     </Card>
--- src/components/Movie/MovieGrid.tsx
+++ src/components/Movie/MovieGrid.tsx
@@ -5,7 +5,7 @@ import { MovieCard } from '@components'
 
 export const MovieGrid = ({ category }: { category: string }) => {
   return (
-    <div className="Movie__grid container mx-auto mt-5 px-5 pb-5 sm:p-5">
+    <section className="movie__grid container mx-auto mt-5 px-5 pb-5 sm:p-5">
       <div className="flex justify-between items-center mb-2">
         <Typography variant="h3" className="mr-2 cursor-pointer py-1.5 font-bold text-pink-400 capitalize   ">
           {category}
@@ -24,10 +24,16 @@ export const MovieGrid = ({ category }: { category: string }) => {
           {Array(20)
             .fill({})
             .map((_, index) => (
-              <MovieCard key={index} />
+              <MovieCard
+                key={index}
+                className="test bg-inherit relative grid  w-full h-full rounded-none items-end overflow-hidden text-center"
+                classHeader="relative pt-[150%] inset-0 m-0 w-full rounded-2xl bg-[url('https://image.tmdb.org/t/p/original//vZloFAK7NmvMGKE7VkF5UHaz0I.jpg')] bg-cover"
+                classFooter="flex items-center justify-between w-full p-2"
+                textFooter='2023'
+              />
             ))}
         </div>
       </div>
-    </div>
+    </section>
   )
 }
