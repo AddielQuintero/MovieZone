e52e52da965a71abd2f7ceb92f6e6c8c0122865c
atamayo <atamayoquintero@gmail.com>
2023-06-30 09:31:00 -0500

    feat: add originalImage and w500Image variables to config

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -1,4 +1,6 @@
 export const CONFIG = {
   API_KEY: import.meta.env.VITE_API_KEY,
   API_BASE: import.meta.env.VITE_API_BASE,
-};
+  originalImage: (imgPath: string | undefined) => `https://image.tmdb.org/t/p/original${imgPath}`,
+  w500Image: (imgPath: string | undefined) => `https://image.tmdb.org/t/p/w500${imgPath}`,
+}
