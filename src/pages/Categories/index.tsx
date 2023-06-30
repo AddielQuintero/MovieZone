2b124360b1f213c8b34a04019104c9c3e0067352
atamayo <atamayoquintero@gmail.com>
2023-06-30 09:45:28 -0500

    fix: update categories page

--- src/pages/Categories/index.tsx
+++ src/pages/Categories/index.tsx
@@ -1,3 +1,5 @@
+import { MovieGrid } from "@components"
+
 export const Categories = () => {
-  return <section className="container mx-auto mt-5 px-5 pb-5 sm:p-5">Categories</section>
+  return <MovieGrid category='Categories' movies={[]}/>
 }
