f2d3789a9adc5a2ca909f3fa986adfae700cd116
atamayo <atamayoquintero@gmail.com>
2023-07-04 09:31:30 -0500

    fix: improve movieGenres component  and update categories page

--- src/components/Movie/MovieDetailComponents/MovieGenres.tsx
+++ src/components/Movie/MovieDetailComponents/MovieGenres.tsx
@@ -1,17 +1,22 @@
 import { MovieGenreSkeleton } from '@components'
 import { Chip, Typography } from '@material-tailwind/react'
 import { MovieGenreProps } from '@types'
+import { Link } from 'react-router-dom'
 
-export const MovieGenres = ({ genres, className, classListGenres, classTypography, variant }: MovieGenreProps) => {
+export const MovieGenres = ({ genres, className, classListGenres, classTypography, classChip, redirect, variant }: MovieGenreProps) => {
   return (
     <div className={className}>
-      <Typography className={classTypography} variant={variant}>Categories</Typography>
+      <Typography className={classTypography} variant={variant}>
+        Categories
+      </Typography>
       {!genres.length ? (
         <MovieGenreSkeleton value={20} />
       ) : (
         <div className={classListGenres}>
           {genres.map((genre, index) => (
-            <Chip className="bg-blue-gray-800" size="lg" value={genre.name} key={index} />
+            <Link to={redirect ? `/categories/${genre.id}` : '#'} key={index}>
+              <Chip className={classChip} size="lg" value={genre.name}  />
+            </Link>
           ))}
         </div>
       )}
