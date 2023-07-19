676c1cb58ce1c5839884f1c28cc10e36c0bd04c0
atamayo <atamayoquintero@gmail.com>
2023-07-19 11:55:43 -0500

    feat: add UpButton component

---
+++ src/components/common/UpButton.tsx
@@ -0,0 +1,33 @@
+import { useEffect } from 'react'
+import { ChevronUpIcon } from '@heroicons/react/24/solid'
+import { IconicButton } from '@components'
+
+export const UpButton = () => {
+  useEffect(() => {
+    window.addEventListener('scroll', showUpButton)
+    return () => {
+      window.removeEventListener('scroll', showUpButton)
+    }
+  }, [])
+
+  const showUpButton = () => {
+    const upButton = document.querySelector('.upButton') as HTMLElement | null
+    const shouldDisplay = document.documentElement.scrollTop >= window.innerHeight
+    if (upButton) {
+      upButton.style.display = shouldDisplay ? 'flex' : 'none'
+    }
+  }
+
+  const handleScrollTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  }
+  return (
+    <IconicButton
+      className="upButton z-10 !fixed bottom-8 right-8 hidden items-center gap-3 px-4 py-1.5 sm:py-2 text-pink-400 border border-pink-400 shadow-pink-500/20 hover:shadow-pink-500/20"
+      classIcon="h-5 w-5"
+      color="white"
+      IconComponent={ChevronUpIcon}
+      onClick={handleScrollTop}
+    />
+  )
+}
