9eb14b08bb3c843a0a21846aa24b12e2164f5a58
atamayo <atamayoquintero@gmail.com>
2023-07-19 11:55:20 -0500

    fix: update IconicButton component

--- src/components/common/IconicButton.tsx
+++ src/components/common/IconicButton.tsx
@@ -1,11 +1,11 @@
 import { Button, Typography } from '@material-tailwind/react'
 import { IconicButtonProps } from '@types'
 
-export const IconicButton = ({ className, classTypography, classIcon, color, IconComponent, name }: IconicButtonProps) => {
+export const IconicButton = ({ className, classTypography, classIcon, color, IconComponent, name, refButton, onClick }: IconicButtonProps) => {
   return (
-    <Button className={className} color={color}>
+    <Button className={className} color={color} onClick={onClick} ref={refButton}>
       <IconComponent className={classIcon} strokeWidth={2} />
-      <Typography className={classTypography}>{name}</Typography>
+      {name && <Typography className={classTypography}>{name}</Typography>}
     </Button>
   )
 }
