1c9ddc4ed2464e509f73f4b1618a038e10737908
atamayo <atamayoquintero@gmail.com>
2023-07-27 17:09:32 -0500

    feat: add custom color to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@
 const withMT = require('@material-tailwind/react/utils/withMT')
 
 module.exports = withMT({
+  darkMode: 'class',
   // module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 
@@ -28,9 +29,24 @@ module.exports = withMT({
 
       '4xl': '2560px',
       'max-4xl': { max: '2560px' },
-      
+
       '5xl': '3840px',
       'max-5xl': { max: '3840px' },
+    },
+    colors: {
+      slate: {
+        50: '#f8fafc',
+        100: '#f1f5f9',
+        200: '#e2e8f0',
+        300: '#cbd5e1',
+        400: '#94a3b8',
+        500: '#64748b',
+        600: '#475569',
+        700: '#334155',
+        800: '#1e293b',
+        900: '#0f172a',
+        950: '#1A1F26',
+      },
     },
     extend: {},
   },
