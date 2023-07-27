a2f9e29834f0c397094323d2b721d03df1a5a5b4
atamayo <atamayoquintero@gmail.com>
2023-07-27 17:59:03 -0500

    fix: update dark theme in noFound page

--- src/pages/noFound.tsx
+++ src/pages/noFound.tsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom'
 export const NotFound = () => {
   return (
     <>
-      <main className="grid h-screen place-items-center bg-white dark:bg-slate-700/95 px-6 lg:px-8">
+      <main className="grid h-screen place-items-center bg-white dark:bg-slate-950 px-6 lg:px-8">
         <div className="text-center">
           <p className="text-base font-semibold text-indigo-600 dark:text-gray-200">404</p>
           <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-100 sm:text-5xl">Page not found</h1>
-          <p className="mt-6 text-base leading-7 text-gray-600 dark:text-gray-300">
+          <p className="mt-6 text-base leading-7 text-gray-600 dark:text-gray-400">
             Sorry, we couldn’t find the page you’re looking for.
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
